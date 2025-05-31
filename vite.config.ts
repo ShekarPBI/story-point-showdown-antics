0cd06588c47b303449eeb59e07fb2d8764eee4ce
ShekarPBI <saishekar111@gmail.com>
2025-05-31 09:54:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
+import path from "path"; import { componentTagger } from "lovable-tagger";
+export default defineConfig(({ }) => ({ 
+  server: { 
     host: "::",
     port: 8080,
-  },
-  plugins: [
+  }, plugins: [ 
     react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  build: { outDir: 'docs', },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src"), }, },}));
